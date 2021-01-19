1d9fc38236e2aa1a8f201637110898a3ea631355
Anugrah Singhal <anugrahsinghal1@gmail.com>
2021-01-19 16:08:52 +0530

    Update module name to DEPLOYMENT

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-  // TODO: change your profile information here
-  name: "Ashok Kumar",
-  greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+    // TODO: change your profile information here
+    name: "Anugrah Singhal",
+    greeting: "Hey 👋",
+    greetingDescription: "I'm Anugrah Singhal and I'm a Software Engineer!",
+    githubUrl: "https://github.com/anugrahsinghal",
+    linkedinUrl: "https://www.linkedin.com/in/anugrah-singhal",
+    cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
 };
